268cf05778b34036f90be9fc31a3d854dab4ebf5
Bharadwaj Duggaraju <bharadwaj.duggaraju@outlook.com>
2021-05-16 22:07:52 -0700

    init client

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.css";
+
+const coreAppStyles = {
+  
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>8-1-Backend-Challange</h1>
+      <p>
+        A suite of backends created for the sole purpose of testing the
+        performance, user-experience and development of creating backends in
+        different programming languages
+      </p>
+      <button>View The Tests</button>
+      <button>View The WriteUp</button>
+    </div>
+  );
+}
+
+export default App;
